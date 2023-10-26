45e9f40ec993fab093b823e0fcc82c19871a7dcb
Cervin <houcervin55@gmail.com>
2023-10-26 12:59:21 +0700

    connect api

--- src/Routes/institutes.jsx
+++ src/Routes/institutes.jsx
@@ -5,21 +5,30 @@ import axios from 'axios'
 import { useState, useEffect } from "react";
 
 export default function Institutes() {
-  const [institutes, setInstitutes] = useState(null);
+ const [data,setData] = useState(null)
 
   useEffect(() => {
     axios.get('http://localhost:3001/univ')
     .then((res)=>{
         setData([...res.data])
-        console.log(res)
+        console.log(res.data)
     })
-    .catch((e)=> console.log(e.message))
+    .catch((e)=> console.log(e))
 },[])
+
+console.log(data)
   return (
     <>  
         <Nav/>
+        
         <div className='flex mx-10 pt-20 gap-5 flex-wrap justify-around'>
-            <HorizontalCard link="https://images.unsplash.com/photo-1581362072978-14998d01fdaa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHVuaXZlcnNpdHl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" />
+          {data && data.map((data)=>{
+            return(
+              <HorizontalCard link="https://images.unsplash.com/photo-1581362072978-14998d01fdaa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHVuaXZlcnNpdHl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" name={data.name} address={data.address}/>
+            )
+          })}
+          
+            <HorizontalCard link="https://images.unsplash.com/photo-1581362072978-14998d01fdaa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHVuaXZlcnNpdHl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"/>
             <HorizontalCard link="https://images.unsplash.com/photo-1581362072978-14998d01fdaa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHVuaXZlcnNpdHl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" />
             <HorizontalCard link="https://images.unsplash.com/photo-1581362072978-14998d01fdaa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHVuaXZlcnNpdHl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" />
             <HorizontalCard link="https://images.unsplash.com/photo-1581362072978-14998d01fdaa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHVuaXZlcnNpdHl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" />
